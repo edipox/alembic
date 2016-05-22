ddcbf4fa5357023f4e05dae0841ea89a96bc5f1e
Luke Imhoff <limhoff@csd.org>
2016-05-21 22:12:03 -0500

    Allow Poison ~> 1.5 or ~> 2.0
    Allow compatibility with projects that haven't ugpraded to Poison 2.0.
    Nothing in the Poison.Encoder implementations is 2.0 specific, so allow
    both major versions.

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule Alembic.Mixfile do
       # formats test output for CircleCI
       {:junit_formatter, "~> 1.0", only: :test},
       # JSON decode and encoding.  Protocols are implemented for Alembic.* structs
-      {:poison, "~> 2.1"}
+      {:poison, "~> 1.5 or ~> 2.0"}
     ]
   end
 

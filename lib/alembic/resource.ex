7fe9365298f193e9791bd3b1f8da25a47661dd38
Luke Imhoff <limhoff@csd.org>
2016-06-27 12:32:18 -0500

    Remove Resource.id

--- lib/alembic/resource.ex
+++ lib/alembic/resource.ex
@@ -5,15 +5,9 @@ defmodule Alembic.Resource do
 
   # Struct
 
-  defstruct id: nil,
-             type: nil
+  defstruct type: nil
 
   # Types
-
-  @typedoc """
-  The ID of a `Resource.t`.  Usually the primary key or UUID for a resource in the server.
-  """
-  @type id :: String.t
 
   @typedoc """
   The type of a `Resource.t`.  Can be either singular or pluralized, althought the JSON API spec examples favor
@@ -22,7 +16,6 @@ defmodule Alembic.Resource do
   @type type :: String.t
 
   @type t :: %__MODULE__{
-               id: id | nil,
                type: type
              }
 
@@ -34,25 +27,18 @@ defmodule Alembic.Resource do
   @spec to_params(t, ToParams.resource_by_id_by_type, ToParams.converted_by_id_by_type) :: ToParams.params
   def to_params(resource, resource_by_id_by_type, converted_by_id_by_type)
 
-  def to_params(%__MODULE__{id: id, type: type},
+  def to_params(%__MODULE__{type: type},
                 resource_by_id_by_type,
                 converted_by_id_by_type) when is_map(resource_by_id_by_type) and is_map(converted_by_id_by_type) do
-    case get_in(converted_by_id_by_type, [type, id]) do
+    case get_in(converted_by_id_by_type, [type]) do
       true ->
-        %{"id" => id}
+        %{}
       nil ->
         params = %{}
 
-        params = case id do
-          nil ->
-            params
-          _ ->
-            Map.put(params, "id", id)
-        end
-
         updated_converted_by_id_by_type = converted_by_id_by_type
                                           |> Map.put_new(type, %{})
-                                          |> put_in([type, id], true)
+                                          |> put_in([type], true)
 
         Map.merge(params, %{})
     end

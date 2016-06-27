20d11f9f28382689ea8c2a192494d3171edbb9f9
Luke Imhoff <limhoff@csd.org>
2016-06-27 12:37:30 -0500

    Drop ToParams

--- lib/alembic/resource.ex
+++ lib/alembic/resource.ex
@@ -1,18 +1,10 @@
 defmodule Alembic.Resource do
-  alias Alembic.ToParams
-
-  @behaviour ToParams
-
   # Struct
 
   defstruct type: nil
 
   # Types
 
-  @typedoc """
-  The type of a `Resource.t`.  Can be either singular or pluralized, althought the JSON API spec examples favor
-  pluralized.
-  """
   @type type :: String.t
 
   @type t :: %__MODULE__{
@@ -21,26 +13,14 @@ defmodule Alembic.Resource do
 
   # Functions
 
-  @spec to_params(t, ToParams.resource_by_id_by_type) :: ToParams.params
+  @spec to_params(t, %{type => t}) :: list | map | nil
   def to_params(resource, resource_by_id_by_type), do: to_params(resource, resource_by_id_by_type, %{})
 
-  @spec to_params(t, ToParams.resource_by_id_by_type, ToParams.converted_by_id_by_type) :: ToParams.params
-  def to_params(resource, resource_by_id_by_type, converted_by_id_by_type)
+  @spec to_params(t, %{type => t}, %{type => boolean}) :: list | map | nil
 
-  def to_params(%__MODULE__{type: type},
+  def to_params(_,
                 resource_by_id_by_type,
                 converted_by_id_by_type) when is_map(resource_by_id_by_type) and is_map(converted_by_id_by_type) do
-    case get_in(converted_by_id_by_type, [type]) do
-      true ->
-        %{}
-      nil ->
-        params = %{}
-
-        updated_converted_by_id_by_type = converted_by_id_by_type
-                                          |> Map.put_new(type, %{})
-                                          |> put_in([type], true)
-
-        Map.merge(params, %{})
-    end
+    %{}
   end
 end

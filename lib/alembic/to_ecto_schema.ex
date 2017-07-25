10868a46dbe19772ec638dc2d464ea246f4bc446
Jeffery Utter <jeff@jeffutter.com>
2017-07-24 20:04:22 -0500

    Support Many-to-Many relatioships

--- lib/alembic/to_ecto_schema.ex
+++ lib/alembic/to_ecto_schema.ex
@@ -122,6 +122,20 @@ defmodule Alembic.ToEctoSchema do
     %{acc | field => associated}
   end
 
+  defp put_association(
+         acc,
+         relationship_params,
+         %Ecto.Association.ManyToMany{cardinality: :many, field: field, related: related}
+       ) do
+    associated = case relationship_params do
+      list when is_list(list) ->
+        Enum.map(list, &to_ecto_schema(&1, related))
+      nil -> nil
+    end
+
+    %{acc | field => associated}
+  end
+
   def put_named_association(acc, params, ecto_schema_module, association_name) do
     relationship_name = to_string(association_name)
 
